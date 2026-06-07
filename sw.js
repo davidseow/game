edcae204b737afddb3eb37cd40953b859a6fc9e2
davidseow <david.seow@gmail.com>
2026-06-07 16:13:26 +0100

    Add icons to service worker asset cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'echorunner-v3';
-const ASSETS = ['./', './index.html', './style.css', './game.js', './manifest.json'];
+const ASSETS = ['./', './index.html', './style.css', './game.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
